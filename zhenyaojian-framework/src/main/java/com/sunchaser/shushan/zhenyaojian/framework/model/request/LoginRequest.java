ff342c34e8f766b8547a32838223d63a43342da7
lilu <admin@lilu.org.cn>
2022-11-05 00:27:03 +0800

    feat: add lombok annotation

--- zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/model/request/LoginRequest.java
+++ zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/model/request/LoginRequest.java
@@ -1,6 +1,9 @@
 package com.sunchaser.shushan.zhenyaojian.framework.model.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * login request
@@ -9,6 +12,9 @@ import lombok.Data;
  * @since JDK8 2022/11/2
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequest {
 
     private String account;
