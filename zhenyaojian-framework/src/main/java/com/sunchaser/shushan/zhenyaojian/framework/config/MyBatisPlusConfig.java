c0bc64d226dfd887dbba347f016156098b1af113
lilu <admin@lilu.org.cn>
2022-11-05 00:26:44 +0800

    feat: config mybatis plus table field fill

--- zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/config/MyBatisPlusConfig.java
+++ zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/config/MyBatisPlusConfig.java
@@ -1,11 +1,19 @@
 package com.sunchaser.shushan.zhenyaojian.framework.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.sunchaser.shushan.zhenyaojian.framework.security.LoginUser;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.time.LocalDateTime;
 
 /**
  * MyBatis Plus 插件配置
@@ -14,14 +22,50 @@ import org.springframework.context.annotation.Configuration;
  * @since JDK8 2022/3/13
  */
 @Configuration
+@RequiredArgsConstructor
 @MapperScan("com.sunchaser.shushan.zhenyaojian.system.repository.mapper")
-public class MyBatisPlusConfig {
+public class MyBatisPlusConfig implements MetaObjectHandler {
 
+    /**
+     * 配置分页插件
+     *
+     * @return MybatisPlusInterceptor
+     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         // 分页查询
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return mybatisPlusInterceptor;
+    }
+
+    /**
+     * 配置执行 insert 语句时的字段自动填充
+     *
+     * @param metaObject MetaObject
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "createUser", String.class, getLoginUsername());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateUser", String.class, getLoginUsername());
+    }
+
+    /**
+     * 配置执行 update 语句时的字段自动填充
+     *
+     * @param metaObject MetaObject
+     */
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateUser", String.class, getLoginUsername());
+    }
+
+    private static String getLoginUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LoginUser loginUser = (LoginUser) authentication.getPrincipal();
+        return loginUser.getUsername();
     }
 }
