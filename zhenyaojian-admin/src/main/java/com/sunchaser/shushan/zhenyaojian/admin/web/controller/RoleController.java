9a7326e7b93e9e6bfe2d1af836af13c18c7f99a5
lilu <admin@lilu.org.cn>
2022-11-05 00:27:38 +0800

    feat: add create role api

--- zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/web/controller/RoleController.java
+++ zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/web/controller/RoleController.java
@@ -1,10 +1,13 @@
 package com.sunchaser.shushan.zhenyaojian.admin.web.controller;
 
+import com.sunchaser.shushan.mojian.base.entity.response.IResponse;
 import com.sunchaser.shushan.mojian.base.entity.response.MultiResponse;
+import com.sunchaser.shushan.zhenyaojian.framework.model.request.CreateRoleRequest;
 import com.sunchaser.shushan.zhenyaojian.system.repository.entity.RoleEntity;
 import com.sunchaser.shushan.zhenyaojian.system.service.RoleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @since JDK8 2022/11/1
  */
 @RestController
-@RequestMapping("/role")
 @RequiredArgsConstructor
 public class RoleController {
 
@@ -22,5 +24,14 @@ public class RoleController {
 
     public MultiResponse<RoleEntity> list() {
         return MultiResponse.success(roleService.list());
+    }
+
+    @PostMapping("/role")
+    public IResponse create(@RequestBody CreateRoleRequest request) {
+        RoleEntity role = new RoleEntity();
+        role.setCode(request.getCode());
+        role.setName(request.getName());
+        roleService.save(role);
+        return IResponse.SUCCESS;
     }
 }
