ab09d884a7ed41f57cb436f16a0c2940776a1bfe
lilu <admin@lilu.org.cn>
2022-11-05 23:21:58 +0800

    feat: enable mojian framework jackson auto config

--- zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/ZhenYaoJianApplication.java
+++ zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/ZhenYaoJianApplication.java
@@ -1,5 +1,6 @@
 package com.sunchaser.shushan.zhenyaojian.admin;
 
+import com.sunchaser.shushan.mojian.web.annotation.EnableMjJackson;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan("com.sunchaser.shushan.zhenyaojian")
+@EnableMjJackson
 public class ZhenYaoJianApplication {
 
     public static void main(String[] args) {
