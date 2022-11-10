341932353ef4e3779ea2458f3dde8b6a0ac66a48
lilu <admin@lilu.org.cn>
2022-11-11 01:46:21 +0800

    feat: add comments

--- zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/config/property/JwtProperties.java
+++ zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/config/property/JwtProperties.java
@@ -17,16 +17,34 @@ import static org.springframework.security.config.Elements.JWT;
 @ConfigurationProperties(prefix = JWT)
 public class JwtProperties {
 
+    /**
+     * jwt 过期时间
+     */
     private Long expiration = 24 * 60 * 3600L;
 
+    /**
+     * jwt 签名类型
+     */
     private SignType signType = SignType.SECRET;
 
+    /**
+     * 签名类型为秘钥时的秘钥
+     */
     private String secret;
 
+    /**
+     * 签名类型为公私钥时的公钥路径
+     */
     private String publicKeyLocation;
 
+    /**
+     * 签名类型为公私钥时的私钥路径
+     */
     private String privateKeyLocation;
 
+    /**
+     * 签名类型
+     */
     public enum SignType {
 
         /**
