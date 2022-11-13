d7823b1f35f870c8b3f4d6fe52f7361fb4e70e91
lilu <admin@lilu.org.cn>
2022-11-14 00:29:32 +0800

    fix: Validated annotation's location and group

--- zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/web/controller/PermissionController.java
+++ zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/web/controller/PermissionController.java
@@ -2,6 +2,7 @@ package com.sunchaser.shushan.zhenyaojian.admin.web.controller;
 
 import com.sunchaser.shushan.mojian.base.entity.response.IResponse;
 import com.sunchaser.shushan.mojian.base.entity.response.MultiResponse;
+import com.sunchaser.shushan.mojian.web.validation.groups.Update;
 import com.sunchaser.shushan.zhenyaojian.framework.model.request.PermissionOps;
 import com.sunchaser.shushan.zhenyaojian.framework.model.response.PermissionDetailTreeNode;
 import com.sunchaser.shushan.zhenyaojian.framework.model.response.PermissionTreeNode;
@@ -25,19 +26,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
-@Validated
 public class PermissionController {
 
     private final PermissionService permissionService;
 
     @PostMapping("/permission")
-    public IResponse createPermission(@RequestBody PermissionOps request) {
+    public IResponse createPermission(@Validated @RequestBody PermissionOps request) {
         permissionService.createPermission(request);
         return IResponse.ofSuccess();
     }
 
     @PatchMapping("/permission")
-    public IResponse updatePermission(@RequestBody PermissionOps request) {
+    public IResponse updatePermission(@Validated({Update.class}) @RequestBody PermissionOps request) {
         permissionService.updatePermission(request);
         return IResponse.ofSuccess();
     }
