d24abcb6bc65ab9f0c5bea6767476bf54a914ee2
lilu <admin@lilu.org.cn>
2022-11-14 20:46:58 +0800

    feat: search by name and delete by id

--- zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/service/PermissionService.java
+++ zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/service/PermissionService.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 import com.google.common.base.Preconditions;
+import com.sunchaser.shushan.mojian.base.util.Optionals;
 import com.sunchaser.shushan.zhenyaojian.framework.enums.PermissionTypeEnum;
 import com.sunchaser.shushan.zhenyaojian.framework.mapstruct.PermissionMapstruct;
 import com.sunchaser.shushan.zhenyaojian.framework.model.request.PermissionOps;
@@ -51,10 +53,20 @@ public class PermissionService extends ServiceImpl<PermissionMapper, PermissionE
             Preconditions.checkArgument(PermissionTypeEnum.isNotButton(parentPermission.getType()), "按钮下不能添加子菜单");
         }
         PermissionEntity permission = permissionMapstruct.convert(create);
+        // 此处 parentId 不能被删除
         this.save(permission);
     }
 
     public void updatePermission(PermissionOps update) {
+        Long id = update.getId();
+        PermissionEntity exist = this.getById(id);
+        LambdaQueryWrapper<PermissionEntity> wrapper = Wrappers.<PermissionEntity>lambdaQuery()
+                .eq(PermissionEntity::getParentId, id);
+        long childrenCount = this.count(wrapper);
+        // 如果存在子菜单且修改了 permission 的类型 type
+        // 做法一：将所有子菜单的 parentId 修改为 当前菜单的 parentId（需批量更新，业务上较为复杂）
+        // 做法二：提示用户存在子菜单不允许修改菜单类型（√）
+        Preconditions.checkArgument(!(SqlHelper.retBool(childrenCount) && !exist.getType().equals(update.getType())), "该菜单下存在子菜单，暂不允许修改菜单类型~");
         this.updateById(permissionMapstruct.convert(update));
     }
 
@@ -62,33 +74,11 @@ public class PermissionService extends ServiceImpl<PermissionMapper, PermissionE
         return TreeBuilder.build(queryCurrentUserPermissions(), permissionMapstruct::convertToPermissionDetailTreeNode);
     }
 
-    /**
-     * 获取当前登录用户拥有的权限
-     * userId -> roles -> permissions
-     *
-     * @return 权限列表
-     */
-    public List<PermissionEntity> queryCurrentUserPermissions() {
-        LoginUser loginUser = SecurityUtils.getLoginUser();
-        if (loginUser.isSuperAdmin()) {
-            // 内部超级管理员查询所有权限
-            return this.list();
-        }
-        LambdaQueryWrapper<UserRoleEntity> userRoleWrapper = Wrappers.<UserRoleEntity>lambdaQuery()
-                .eq(UserRoleEntity::getUserId, loginUser.getUserId());
-        // userId -> roles
-        List<UserRoleEntity> userRoles = userRoleService.list(userRoleWrapper);
-        List<Long> roleIds = Streams.mapToList(userRoles, UserRoleEntity::getRoleId);
-        LambdaQueryWrapper<RolePermissionEntity> rolePermissionWrapper = Wrappers.<RolePermissionEntity>lambdaQuery()
-                .in(RolePermissionEntity::getRoleId, roleIds);
-        // roles -> permissions
-        List<RolePermissionEntity> rolePermissions = rolePermissionService.list(rolePermissionWrapper);
-        List<Long> permissionIds = Streams.mapToList(rolePermissions, RolePermissionEntity::getPermissionId);
-        LambdaQueryWrapper<PermissionEntity> permissionWrapper = Wrappers.<PermissionEntity>lambdaQuery()
-                .in(PermissionEntity::getId, permissionIds);
-        return this.list(permissionWrapper);
+    public List<PermissionDetailTreeNode> permissionsSearch(String name) {
+        LambdaQueryWrapper<PermissionEntity> condition = Wrappers.<PermissionEntity>lambdaQuery()
+                .likeRight(PermissionEntity::getName, name);
+        return TreeBuilder.build(queryCurrentUserPermissionsByCondition(condition), permissionMapstruct::convertToPermissionDetailTreeNode);
     }
-
 
     /**
      * 新建菜单时：
@@ -105,6 +95,53 @@ public class PermissionService extends ServiceImpl<PermissionMapper, PermissionE
                 permissionMapstruct::convertToPermissionTreeNode,
                 (root, children) -> PermissionFilterEnum.valueOf(filter).filter(root)
         );
+    }
+
+    public void deletePermission(Long id) {
+        LambdaQueryWrapper<PermissionEntity> wrapper = Wrappers.<PermissionEntity>lambdaQuery()
+                .eq(PermissionEntity::getParentId, id);
+        long count = this.count(wrapper);
+        Preconditions.checkArgument(!SqlHelper.retBool(count), "该菜单下存在子菜单，不允许删除~");
+        LambdaQueryWrapper<RolePermissionEntity> rpWrapper = Wrappers.<RolePermissionEntity>lambdaQuery()
+                .eq(RolePermissionEntity::getPermissionId, id);
+        long rpCount = rolePermissionService.count(rpWrapper);
+        Preconditions.checkArgument(!SqlHelper.retBool(rpCount), "该菜单已分配给相关角色，不允许删除~");
+        this.removeById(id);
+    }
+
+    /**
+     * 获取当前登录用户拥有的权限
+     * userId -> roles -> permissions
+     *
+     * @return 权限列表
+     */
+    public List<PermissionEntity> queryCurrentUserPermissions() {
+        return queryCurrentUserPermissionsByCondition();
+    }
+
+    public List<PermissionEntity> queryCurrentUserPermissionsByCondition() {
+        return queryCurrentUserPermissionsByCondition(Wrappers.lambdaQuery());
+    }
+
+    public List<PermissionEntity> queryCurrentUserPermissionsByCondition(LambdaQueryWrapper<PermissionEntity> condition) {
+        LoginUser loginUser = SecurityUtils.getLoginUser();
+        condition = Optionals.of(condition, Wrappers.lambdaQuery());
+        if (loginUser.isSuperAdmin()) {
+            // 内部超级管理员查询所有权限
+            return this.list(condition);
+        }
+        LambdaQueryWrapper<UserRoleEntity> urWrapper = Wrappers.<UserRoleEntity>lambdaQuery()
+                .eq(UserRoleEntity::getUserId, loginUser.getUserId());
+        // userId -> roles
+        List<UserRoleEntity> userRoles = userRoleService.list(urWrapper);
+        List<Long> roleIds = Streams.mapToList(userRoles, UserRoleEntity::getRoleId);
+        LambdaQueryWrapper<RolePermissionEntity> rpWrapper = Wrappers.<RolePermissionEntity>lambdaQuery()
+                .in(RolePermissionEntity::getRoleId, roleIds);
+        // roles -> permissions
+        List<RolePermissionEntity> rolePermissions = rolePermissionService.list(rpWrapper);
+        List<Long> permissionIds = Streams.mapToList(rolePermissions, RolePermissionEntity::getPermissionId);
+        condition.in(PermissionEntity::getId, permissionIds);
+        return this.list(condition);
     }
 
     public enum PermissionFilterEnum {
