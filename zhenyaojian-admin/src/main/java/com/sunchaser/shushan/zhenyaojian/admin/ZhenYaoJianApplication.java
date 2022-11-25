49940df9922a8dcbef2ea34db44cb52366745849
lilu <admin@lilu.org.cn>
2022-11-25 21:13:06 +0800

    feat: add EnableAdminServer

--- zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/ZhenYaoJianApplication.java
+++ zhenyaojian-admin/src/main/java/com/sunchaser/shushan/zhenyaojian/admin/ZhenYaoJianApplication.java
@@ -2,6 +2,7 @@ package com.sunchaser.shushan.zhenyaojian.admin;
 
 import com.sunchaser.shushan.mojian.web.annotation.EnableMjJackson;
 import com.sunchaser.shushan.mojian.web.annotation.EnableValidatorFailFast;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.sunchaser.shushan.zhenyaojian")
 @EnableMjJackson
 @EnableValidatorFailFast
+@EnableAdminServer
 public class ZhenYaoJianApplication {
 
     public static void main(String[] args) {
