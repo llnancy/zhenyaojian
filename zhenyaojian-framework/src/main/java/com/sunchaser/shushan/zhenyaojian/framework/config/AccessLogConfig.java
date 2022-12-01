f2c292eb2201ad0bc16c17ff099d83cf56533b33
lilu <admin@lilu.org.cn>
2022-12-02 00:57:06 +0800

    feat: add AccessLog event listener

--- zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/config/AccessLogConfig.java
+++ zhenyaojian-framework/src/main/java/com/sunchaser/shushan/zhenyaojian/framework/config/AccessLogConfig.java
@@ -1,14 +1,11 @@
 package com.sunchaser.shushan.zhenyaojian.framework.config;
 
-import com.sunchaser.shushan.mojian.log.entity.AccessLogBean;
 import com.sunchaser.shushan.mojian.log.event.AccessLogEventListener;
 import com.sunchaser.shushan.zhenyaojian.framework.mapstruct.SystemLogMapstruct;
 import com.sunchaser.shushan.zhenyaojian.system.repository.mapper.SystemLogMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Consumer;
 
 /**
  * @author sunchaser admin@lilu.org.cn
