1edf218d6f4ae6cd65f9928951b1df4aee395545
lilu <admin@lilu.org.cn>
2022-12-02 00:56:12 +0800

    feat: update zyj_system_log table fields

--- zhenyaojian-system/src/main/java/com/sunchaser/shushan/zhenyaojian/system/repository/mapper/SystemLogMapper.java
+++ zhenyaojian-system/src/main/java/com/sunchaser/shushan/zhenyaojian/system/repository/mapper/SystemLogMapper.java
@@ -7,7 +7,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * 系统日志记录表 Mapper 接口
  *
  * @author sunchaser admin@lilu.org.cn
- * @since JDK8 2022/11/30
+ * @since JDK8 2022/12/02
  */
 public interface SystemLogMapper extends BaseMapper<SystemLogEntity> {
 }
