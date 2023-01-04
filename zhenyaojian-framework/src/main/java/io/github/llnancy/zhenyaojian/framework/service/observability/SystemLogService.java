e46d98d7bfe6eaf67ccc4f0e2c08e1672c35cfd2
lilu <admin@lilu.org.cn>
2023-01-04 20:41:11 +0800

    feat: query logs order by id desc

--- zhenyaojian-framework/src/main/java/io/github/llnancy/zhenyaojian/framework/service/observability/SystemLogService.java
+++ zhenyaojian-framework/src/main/java/io/github/llnancy/zhenyaojian/framework/service/observability/SystemLogService.java
@@ -5,13 +5,13 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import io.github.llnancy.mojian.base.entity.response.MultiPageResponse;
+import io.github.llnancy.mojian.log.enums.AccessType;
 import io.github.llnancy.zhenyaojian.framework.mapstruct.SystemLogMapstruct;
 import io.github.llnancy.zhenyaojian.framework.model.request.SystemLogPageRequest;
 import io.github.llnancy.zhenyaojian.framework.model.response.SystemLogResponse;
 import io.github.llnancy.zhenyaojian.system.repository.entity.SystemLogEntity;
 import io.github.llnancy.zhenyaojian.system.repository.mapper.SystemLogMapper;
-import io.github.llnancy.mojian.base.entity.response.MultiPageResponse;
-import io.github.llnancy.mojian.log.enums.AccessType;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -38,7 +38,8 @@ public class SystemLogService extends ServiceImpl<SystemLogMapper, SystemLogEnti
         String type = request.getType();
         LambdaQueryWrapper<SystemLogEntity> wrapper = Wrappers.<SystemLogEntity>lambdaQuery()
                 .eq(StringUtils.isNotBlank(type), SystemLogEntity::getType, type)
-                .ne(StringUtils.isBlank(type), SystemLogEntity::getType, AccessType.LOGIN.toString());
+                .ne(StringUtils.isBlank(type), SystemLogEntity::getType, AccessType.LOGIN.toString())
+                .orderByDesc(SystemLogEntity::getId);
         Page<SystemLogEntity> page = Page.of(request.getPageNo(), request.getPageSize());
         page = this.getBaseMapper().selectPage(page, wrapper);
         return MultiPageResponse.success(page, systemLogMapstruct::convert);
